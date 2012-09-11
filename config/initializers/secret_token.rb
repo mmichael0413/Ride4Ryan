77e3b93840651dffae3cde33970048f56746cf71
Mike Michaelevich <mmichael0413@gmail.com>
2012-09-10 21:25:02 -0400

    add it all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ride4Ryan::Application.config.secret_token = '94b5f64fa6301c5523325db8785173e01655a327195967a40886747cc6a1b1d0993ba77f566e9570b92e0209ff31806ece858fd15564d86482ea91506a70ed7a'
