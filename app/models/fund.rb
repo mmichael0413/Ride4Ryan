194535054b22b22dde6e526a3fb3326a69a80121
Mike Michaelevich <mmichael0413@gmail.com>
2012-09-15 12:32:29 -0400

    add attr_accesible fields to all models and start writing function for timeslots

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -4,9 +4,16 @@ class Fund < ActiveRecord::Base
   validates_presence_of :title, :headline, :content
   
   belongs_to :admin
-  has_many :days
-  has_many :products
-  has_many :sponsors
+  has_many :days, :dependent => :destroy
+  has_many :products, :dependent => :destroy
+  has_many :sponsors, :dependent => :destroy
   has_many :pledges
   has_many :timeslots
+  
+  #Calculate number of available time slots based on data from Day table
+  def self.number_of_timeslots_available(fund)
+    start_time = Day.where(:fund_id => fund).map(:start_time)
+    return start_time
+  end
+  
 end
