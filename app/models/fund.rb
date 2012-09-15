d84d6903212fa3f56e4be6eae061743cadb9ed6c
Mike Michaelevich <mmichael0413@gmail.com>
2012-09-15 11:11:59 -0400

    add fund model validation

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,6 +1,8 @@
 class Fund < ActiveRecord::Base
   attr_accessible :title, :headline, :content
   
+  validates_presence_of :title, :headline, :content
+  
   belongs_to :admin
   has_many :days
   has_many :products
