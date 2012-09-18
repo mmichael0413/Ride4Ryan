26eb3a877cdafd950076c6128493c323d63446ad
Mike Michaelevich <mmichael0413@gmail.com>
2012-09-18 18:50:30 -0400

    minor changes, switching to new branch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @fund = Fund.find_by_id(1)
+    @pledge = Pledge.new
   end
 end
--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,7 @@
 class PledgesController < ApplicationController
+  def create
+      @idea = Idea.find(params[:idea_id])
+      @bubble = @idea.bubbles.create(params[:bubble])
+      redirect_to idea_path(@idea)
+  end
 end
