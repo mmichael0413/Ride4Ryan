9baeecd5d1a8d2affbfef81c46f15ce167f99688
Mike Michaelevich <mmichael0413@gmail.com>
2012-09-22 08:49:08 -0400

    remove timeslot code from home index view

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,5 +1,4 @@
 class Pledge < ActiveRecord::Base
-  #before_create :determine_type
   after_create :determine_type
   
   attr_accessible :first_name, :last_name, :shipping_address1, :shipping_address2, :shipping_city, :shipping_state
@@ -8,27 +7,27 @@ class Pledge < ActiveRecord::Base
   belongs_to :fund
   has_many :orders
   
-  def determine_type
-    #If the pledge donates at least $30, and does not opt_out, and is not registering for spinning
-    #then create a new order
-    if self.timeslot
-      self.amount = self.fund.registration_fee
-    else
-      if self.amount > 29 && self.opt_out == false && self.timeslot.blank?
-        order = Order.new
-        order.pledge_id = self.id
-        
-          #Check the amount from new pledge form to determine product id
-          if self.amount < 50
-            order.product_id = 1
-          elsif self.amount < 100
-            order.product_id = 2
-          else
-            order.product_id = 3
-          end
-          
-        order.save
-      end
-    end
-  end
+  #def determine_type
+  #  #If the pledge donates at least $30, and does not opt_out, and is not registering for spinning
+  #  #then create a new order
+  #  if self.timeslot
+  #    self.amount = self.fund.registration_fee
+  #  else
+  #    if self.amount > 29 && self.opt_out == false && self.timeslot.blank?
+  #      order = Order.new
+  #      order.pledge_id = self.id
+  #      
+  #        #Check the amount from new pledge form to determine product id
+  #        if self.amount < 50
+  #          order.product_id = 1
+  #        elsif self.amount < 100
+  #          order.product_id = 2
+  #        else
+  #          order.product_id = 3
+  #        end
+  #        
+  #      order.save
+  #    end
+  #  end
+  #end
 end
