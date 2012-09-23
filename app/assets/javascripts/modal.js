c179396b00debd2da0735e34409f051817af6828
Elliot Nash <Elliot@venturemedias.com>
2012-09-23 13:37:52 -0400

    put forms in modals

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 
   $('.modal-link').click(function(){
+    content = $(this).attr("data-target");
+    $('.modal_content').hide();
+    amount = $('input.pledge_amount').val();
+    if(content === "modal_donate"){
+      title = "Almost there!";
+    }else{
+      title = "Spin to win!";
+    }
+    $('.modal_title h2').text(title);
+    $('.donation_amount_text').text(amount);
+    $('input.p_amt').val(amount);
+    $('.'+content).show();
     $('.modal_bg').fadeIn(100);
     $('.modal_box').delay(100).fadeIn(100);
   });
