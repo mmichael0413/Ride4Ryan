e1e891896fed79f6d34be77b094dcb732193cbea
Mike Michaelevich <mmichael0413@gmail.com>
2012-09-26 19:44:49 -0400

    add sendgrid SMTP settings to environment.rb and remove mail.rb initializer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ride4Ryan::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app7538690@heroku.com",
+  :password => "77$v1nka",
+  :domain => "ride4ryan.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- config/initializers/mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :user_name => "app7538690@heroku.com",
-  :password => "88Sv1nka",
-  :domain => "ride4ryan.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
