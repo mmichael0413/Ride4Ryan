c2dd42e2ae25a72bb4a40b631d7eaefb7ef69a90
Mike Michaelevich <mmichael0413@gmail.com>
2012-09-26 19:23:32 -0400

    add sendgrid SMTP settings to mail initializer

---
+++ config/initializers/mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app7538690@heroku.com",
+  :password => "secret",
+  :domain => "ride4ryan.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
