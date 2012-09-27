3181ed146bc9759cc34167bbf56d800912fdd46f
Elliot Nash <Elliot@venturemedias.com>
2012-09-26 20:08:57 -0400

    rmv smtp config

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Ride4Ryan::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => "app7538690@heroku.com",
-  :password => "77$v1nka",
-  :domain => "ride4ryan.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
