c958a4b3e26324dbc5ea012f223d0fb83ce80419
Mike Michaelevich <mmichael0413@gmail.com>
2012-09-28 20:03:43 -0400

    update donation and registration emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,17 +4,11 @@ class UserMailer < ActionMailer::Base
     @pledge = pledge
     @fund = Fund.find(pledge.fund_id)
     @day = Day.find(pledge.day_id)
-    @admin = Admin.find(@fund.admin_id)
-    @url  = "http://example.com/login"
+    @time = @fund.fund_times(@day)
     mail(:to => pledge.email_address, :subject => "Start Stretching!", :from => "registrations@ride4ryan.com")
   end
   
   def donation_email(pledge)
-    @pledge = pledge
-    @fund = Fund.find(pledge.fund_id)
-    @day = Day.find(pledge.day_id)
-    @admin = Admin.find(@fund.admin_id)
-    @url  = "http://example.com/login"
     mail(:to => pledge.email_address, :subject => "You are too kind!", :from => "donations@ride4ryan.com")
   end
 end
